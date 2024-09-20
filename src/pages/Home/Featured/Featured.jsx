2091006904c3aa7bb8a696d2e632a4a4e40e7526
bjoarder7 <bjoarder87@gmail.com>
2024-09-21 01:25:00 +0600

    testimonials section added

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -7,12 +7,12 @@ import "./Featured.css";
 
 const Featured = () => {
     return (
-        <section className="featuredImage text-white pt-4 my-16">
+        <section className="featuredImage bg-fixed text-white pt-4 my-16">
             <SectionTitle
             subHeading={"Check it out"}
             heading={"Featured Item"}
             ></SectionTitle>
-            <div className="flex justify-center items-center pb-24 pt-12 px-36">
+            <div className="flex justify-center items-center bg-slate-500 bg-opacity-40 pb-24 pt-12 px-36">
                 <div className="w-2/3">
                     <img src={featuredImg} alt="" />
                 </div>
@@ -20,7 +20,7 @@ const Featured = () => {
                     <p>Sept 20, 2024</p>
                     <h3 className="uppercase">Where can get some?</h3>
                     <p className="text-justify">Food is any substance consumed by an organism for nutritional support. Food is usually of plant, animal, or fungal origin and contains essential nutrients</p>
-                    <button className="btn btn-outline">Order now</button>
+                    <button className="btn btn-outline border-0 border-b-4">Order now</button>
                 </div>
             </div>
             
