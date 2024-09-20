418cb1ee65825c7568a99ce074a50d17e86e7944
bjoarder7 <bjoarder87@gmail.com>
2024-09-20 21:08:07 +0600

    section title added

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,10 @@
 
 
-const SectionTitle = () => {
+const SectionTitle = ({subHeading, heading}) => {
     return (
-        <div>
-            
+        <div className="mx-auto md:w-1/3 text-center my-8">
+            <p className="text-orange-500 pb-2">--- {subHeading} ---</p>
+            <h3 className="text-3xl uppercase border-y-4 py-4">{heading}</h3>
         </div>
     );
 };
