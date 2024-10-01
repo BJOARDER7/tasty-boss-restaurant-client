e44b961cdb052fc02fd9c634d461a1c61fdf625c
bjoarder7 <bjoarder87@gmail.com>
2024-10-01 08:06:56 +0600

    firebase config secret

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -10,11 +10,11 @@ const FoodCard = ({item}) => {
         />
       </figure>
       <p className="absolute right-0 bg-slate-900 text-white px-4 mt-4 mr-4">${price}</p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p className="">{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400">Add to Cart</button>
         </div>
       </div>
     </div>
